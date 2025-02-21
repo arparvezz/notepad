31bf02eeb6af38e76dc959825d13936c63017e7f
arparvezz <arparvez50@gmail.com>
2025-02-21 12:38:22 +0600

    Fixed some issues 2

--- script.js
+++ script.js
@@ -56,6 +56,24 @@ darkModeBtn.addEventListener('click',function(e){
     document.documentElement.style.setProperty('--content-bg-color', newContentBgColor);
     document.documentElement.style.setProperty('--content-color', newContentColor);
     document.documentElement.style.setProperty('--border-color', newBorderColor);
+  }else{
+     // Get the current color value
+     let currentBodyColor = getComputedStyle(document.documentElement).getPropertyValue('--body-bg-color').trim();
+     let currentContentBgColor = getComputedStyle(document.documentElement).getPropertyValue('--content-bg-color').trim();
+     let currentContentColor = getComputedStyle(document.documentElement).getPropertyValue('--content-color').trim();
+     let currentBorderColor = getComputedStyle(document.documentElement).getPropertyValue('--border-color').trim();
+   
+     // Toggle between two colors
+     let newBodyColor = currentBodyColor === '#121212' ? '#efefef' : '#121212';
+     let newContentBgColor = currentContentBgColor === '#292929' ? '#fff' : '#292929';
+     let newContentColor = currentContentColor === '#e2e2e2' ? '#000' : '#e2e2e2';
+     let newBorderColor = currentBorderColor === '#5f5f5f' ? '#efefef' : '#5f5f5f';
+   
+     // Update the CSS variable
+     document.documentElement.style.setProperty('--body-bg-color', newBodyColor);
+     document.documentElement.style.setProperty('--content-bg-color', newContentBgColor);
+     document.documentElement.style.setProperty('--content-color', newContentColor);
+     document.documentElement.style.setProperty('--border-color', newBorderColor);
   }
    
 })
